9acf04355c6e33c3a0d15f95454ad7a7b884e56b
Ensar Kahraman <148242564+Eensar2008@users.noreply.github.com>
2026-08-24 22:27:31 +0200

    feat: list products

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
- console.log("Hello, welcome to the JavaScript Console Shop!");
+console.log("Hello, welcome to the JavaScript Console Shop!");
 
- const products = [
+const products = [
     {
-    name: "Laptop",
-    price: 799.99,
-    stokQuanity: 10,
+        name: "Laptop",
+        price: 799.99,
+        stokQuanity: 10
+    },
+    {
+        name: "Phone",
+        price: 499.99,
+        stokQuanity: 20
     }
 ];
+
+function showProducts() {
+    console.log("Available Products:");
+
+    products.forEach(function(product) {
+        console.log(
+            `- ${product.name}: $${product.price} (Stock: ${product.stokQuanity})`
+        );
+    });
+}
+
+showProducts();
